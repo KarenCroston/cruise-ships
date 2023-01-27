ed3691612aabc7af2bc3ebfc19195bd2e9dc30a5
Karen Croston <karencroston@hotmail.co.uk>
2023-01-27 10:50:08 +0000

    add set sail

--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,10 @@
 class Ship {
     constructor() {
         this.startingPort = 'Dover';
+        this.previousPort = null;
     }
-}
+    setSail() {
+        this.previousPort = this.currentPort;
+        this.startingPort = null;
+    }}
 module.exports = Ship;
